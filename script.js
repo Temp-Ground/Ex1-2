40d19eeb9c347daa48715e198a0f2587e0ec1752
USER <teamjistory@gmail.com>
2023-03-01 14:43:31 +0900

    3

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function createParagraph() {
     const para = document.createElement('p');
     para.textContent = 'You clicked the button!';
     document.body.appendChild(para);
+
 }
 
 const buttons = document.querySelectorAll('button');
